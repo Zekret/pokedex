460be7b9163d27f820f23be6910e499b1b78b27f
Zekret <benjaminconcha5@gmail.com>
2023-02-27 19:27:15 -0300

    update: custom hook getPokes

--- src/components/Pokedex.jsx
+++ src/components/Pokedex.jsx
@@ -1,17 +1,8 @@
-import { useEffect, useState } from "react";
+import { getPokes } from "../hooks/getPokes";
 
 export const Pokedex = () => {
-    const [pokemon, setPokemon] = useState()
+    const { pokemon } = getPokes()
     console.log(pokemon)
-    const getPokemon = async () => {
-        const data = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=12`).then((res) => res.json());
-        setPokemon(data.results)
-    }
-
-
-    useEffect(() => {
-        getPokemon()
-    }, [])
     return (
         <div>
             <h1>Hola</h1>

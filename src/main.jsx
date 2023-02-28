99887ca5966135a986a66ce99bb254eb26878da4
Zekret <benjaminconcha5@gmail.com>
2023-02-28 15:05:18 -0300

    update: router dom & dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -3,21 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import { Home } from './components/Home'
-import { PokeDetail } from './components/PokeDetail'
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Home />
-  },
-  {
-    path: '/:pokeId',
-    element: <PokeDetail />
-  }
-])
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 )

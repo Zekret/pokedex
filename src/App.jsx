b6ba03130745edeacd42391f259c5d1fc61f5bca
Zekret <benjaminconcha5@gmail.com>
2023-02-28 16:05:23 -0300

    update: context & provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import { Navigation } from "./components/Navigation";
+import { PokemonProvider } from "./context/PokemonProvider";
 import { HomePage } from "./pages/HomePage";
 import { PokemonPage } from "./pages/PokemonPage";
 import { SearchPage } from "./pages/SearchPage";
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Navigation />}>
-        <Route index element={<HomePage />} />
-        <Route path='pokemon:id' element={<PokemonPage />} />
-        <Route path='search' element={<SearchPage />} />
-      </Route>
-      <Route path='*' element={<Navigate to='/' />} />
-    </Routes>
+    <PokemonProvider>
+      <Routes>
+        <Route path="/" element={<Navigation />}>
+          <Route index element={<HomePage />} />
+          <Route path="pokemon:id" element={<PokemonPage />} />
+          <Route path="search" element={<SearchPage />} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </PokemonProvider>
   );
 }
 

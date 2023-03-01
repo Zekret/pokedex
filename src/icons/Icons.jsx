76970c8ff44d97bb865f8b01318b7a7e046f22b6
Zekret <benjaminconcha5@gmail.com>
2023-02-28 23:27:28 -0300

    update: components & filter icon

--- src/icons/Icons.jsx
+++ src/icons/Icons.jsx
@@ -16,3 +16,25 @@ export const SearchIcon = () => {
     </svg>
   );
 };
+
+export const FilterIcon = () => {
+  return (
+    <div className="flex items-center gap-4 mb-5 cursor-pointer w-8 h-8 stroke-slate-50">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth="1.5"
+        stroke="currentColor"
+        className="icon"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 10-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75"
+        />
+      </svg>
+      <span>Filtrar</span>
+    </div>
+  );
+};
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from "react";
+import { PokemonList } from "../components/PokemonList";
+import { FilterIcon } from "../icons/Icons";
 
 export const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <>
+      <div className="max-w-7xl m-auto flex">
+        <FilterIcon />
+      </div>
+      <PokemonList />
+    </>
+  );
+};

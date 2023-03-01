24c7dd609d9e8ca375fddf030ad8e9b84d98a63b
Zekret <benjaminconcha5@gmail.com>
2023-03-01 13:18:00 -0300

    update: search page & filtered pokemons

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,17 +1,31 @@
-import React from "react";
-import { Link, Outlet } from "react-router-dom";
+import React, { useContext } from "react";
+import { Link, Outlet, useNavigate } from "react-router-dom";
 import { SearchIcon } from "../icons/Icons";
-import pokedexIcon from '../assets/icon.png'
+import pokedexIcon from "../assets/icon.png";
+import { PokemonContext } from "../context/PokemonContext";
 
 export const Navigation = () => {
+  const { valueSearch, onInputChange, onResetForm } =
+    useContext(PokemonContext);
+  const navigate = useNavigate();
+  const onSearchSubmit = (e) => {
+    e.preventDefault();
+    navigate("/search", {
+      state: valueSearch,
+    });
+
+    onResetForm();
+  };
   return (
     <>
       <header className="flex items-center justify-between py-10 px-10">
         <Link to="/" className="flex flex-row items-center gap-4">
           <img className="w-16" src={pokedexIcon} />
-          <span className="text-3xl font-semibold tracking-widest">Pokedex</span>
+          <span className="text-3xl font-semibold tracking-widest">
+            Pokedex
+          </span>
         </Link>
-        <form className="flex items-center gap-4">
+        <form onSubmit={onSearchSubmit} className="flex items-center gap-4">
           <div className="flex items-center gap-3 border-[1px] border-solid border-slate-300 px-5 py-4 rounded-3xl">
             <SearchIcon />
             <input
@@ -19,8 +33,8 @@ export const Navigation = () => {
               type="search"
               name="valueSearch"
               id=""
-              //   value={valueSearch}
-              //   onChange={onInputChange}
+              value={valueSearch}
+              onChange={onInputChange}
               placeholder="Buscar nombre de pokemon"
             />
           </div>
--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { useLocation } from "react-router-dom";
+import { CardPokemon } from "../components/CardPokemon";
+import { PokemonContext } from "../context/PokemonContext";
 
 export const SearchPage = () => {
+  //Se utiliza para obtener el estado del navigate en el buscador.
+  const location = useLocation();
+
+  const { globalPokemons } = useContext(PokemonContext);
+  const filteredPokemons = globalPokemons.filter((pokemon) =>
+    pokemon.name.includes(location.state.toLowerCase())
+  );
   return (
-    <div>SearchPage</div>
-  )
-}
+    <>
+      <p className="mb-8 text-2xl">
+        Se encontraron <span className="font-bold">{filteredPokemons.length}</span> resultados:
+      </p>
+      <div className="grid grid-cols-4 gap-5 px-10 mt-8">
+        {filteredPokemons.map(pokemon => <CardPokemon pokemon={pokemon} key={pokemon.id} />)}
+      </div>
+    </>
+  );
+};

be43136be82dd993db9dcc700e7d03501536336f
Zekret <benjaminconcha5@gmail.com>
2023-02-28 23:01:25 -0300

    update: getPokemons fetch

--- src/context/PokemonProvider.jsx
+++ src/context/PokemonProvider.jsx
@@ -1,6 +1,64 @@
+import { useState } from "react";
 import { PokemonContext } from "./PokemonContext";
 
+const baseURL = "https://pokeapi.co/api/v2/";
+
 export const PokemonProvider = ({ children }) => {
+  const [allPokemons, setAllPokemons] = useState([]);
+	const [globalPokemons, setGlobalPokemons] = useState([]);
+	const [offset, setOffset] = useState(0);
+  
+  const [loading, setLoading] = useState(true);
+  const [active, setActive] = useState(false);
+
+  // lLamar 50 pokemones a la API
+  const getAllPokemons = async (limit = 50) => {
+    const res = await fetch(
+      `${baseURL}pokemon?limit=${limit}&offset=${offset}`
+    );
+    const data = await res.json();
+
+    const promises = data.results.map(async (pokemon) => {
+      const res = await fetch(pokemon.url);
+      const data = await res.json();
+      return data;
+    });
+    const results = await Promise.all(promises);
+
+    setAllPokemons([...allPokemons, ...results]);
+    setLoading(false);
+  };
+
+  // Llamar todos los pokemones
+  const getGlobalPokemons = async () => {
+    const res = await fetch(`${baseURL}pokemon?limit=100000&offset=0`);
+    const data = await res.json();
+
+    const promises = data.results.map(async (pokemon) => {
+      const res = await fetch(pokemon.url);
+      const data = await res.json();
+      return data;
+    });
+    const results = await Promise.all(promises);
+
+    setGlobalPokemons(results);
+    setLoading(false);
+  };
+
+  // Llamar a un pokemon por ID
+  const getPokemonByID = async (id) => {
+    const res = await fetch(`${baseURL}pokemon/${id}`);
+    const data = await res.json();
+    return data;
+  };
+
+  useEffect(() => {
+    getAllPokemons();
+  }, [offset]);
+
+  useEffect(() => {
+    getGlobalPokemons();
+  }, []);
   return (
     <PokemonContext.Provider
       value={{

0e7f1fea7a43d9627cae4bf738a592f929882a56
Zekret <benjaminconcha5@gmail.com>
2023-03-13 14:13:34 -0300

    fix: img pokemon if not exists

--- src/components/CardPokemon.jsx
+++ src/components/CardPokemon.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { background } from "../utils/BackgroundByType";
+import { Loader } from "./Loader";
 
 export const CardPokemon = ({ pokemon }) => {
   const backgroundSelected = background[pokemon?.types[0]?.type?.name];
@@ -9,15 +10,32 @@ export const CardPokemon = ({ pokemon }) => {
       to={`/pokemon/${pokemon.id}`}
       className="w-full max-w-[350px] rounded-lg flex flex-col cursor-pointer no-underline text-inherit"
     >
-      <div style={{ borderColor: backgroundSelected }} className="flex flex-col border-transparent border-solid border-[1px] rounded-t-lg">
-        <span style={{ color: backgroundSelected, padding: '4px 8px 0px' }} className="text-end">#{pokemon?.id}</span>
-        <img
-          className="w-[90%] h-[140px] object-contain self-center py-2 px-4"
-          src={pokemon.sprites.other.dream_world.front_default}
-          alt={`Pokemon ${pokemon.name}`}
-        />
+      <div
+        style={{ borderColor: backgroundSelected }}
+        className="flex flex-col border-transparent border-solid border-[1px] rounded-t-lg"
+      >
+        <span
+          style={{ color: backgroundSelected, padding: "4px 8px 0px" }}
+          className="text-end"
+        >
+          #{pokemon?.id}
+        </span>
+        {(pokemon?.sprites?.other?.dream_world?.front_default ||
+          pokemon?.sprites?.front_default) && (
+          <img
+            className="w-[90%] h-[140px] object-contain self-center py-2 px-4"
+            src={
+              pokemon?.sprites?.other?.dream_world?.front_default ||
+              pokemon?.sprites?.front_default
+            }
+            alt={`Pokemon ${pokemon.name}`}
+          />
+        )}
       </div>
-      <div style={{ background: backgroundSelected }} className="text-base leading-4 p-4 rounded-b-lg text-center capitalize text-white font-medium">
+      <div
+        style={{ background: backgroundSelected }}
+        className="text-base leading-4 p-4 rounded-b-lg text-center capitalize text-white font-medium"
+      >
         {pokemon?.name}
       </div>
     </Link>
